2652256526785e1a1d0df99a4505051529d5956a
Armia Abdurohman <armia.abdurohman@outlook.com>
2025-07-24 22:15:49 +0700

    fix push button issue

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,13 @@ enum InputDevice : uint8_t
   Sensor4 = 6,
   Sensor5 = 8,
   Sensor6 = 10,
-  PushButton = 12,
+  PushButton = 11,
 };
 
 constexpr uint16_t DISTANCES_MM[] = {0, 300, 600, 800, 1000, 12000};
 constexpr uint16_t SENSOR_COUNT =
     sizeof(DISTANCES_MM) / sizeof(DISTANCES_MM[0]);
+constexpr int Sensors[] = {InputDevice::Sensor1, InputDevice::Sensor2, InputDevice::Sensor3, InputDevice::Sensor4, InputDevice::Sensor5, InputDevice::Sensor6};
 
 unsigned long timeStamps[SENSOR_COUNT];
 float velocities[SENSOR_COUNT - 1];
@@ -26,93 +27,19 @@ constexpr float rho_fluida = 1260;
 constexpr float rho_bola = 7850;
 constexpr float gravity = 9.8;
 
-constexpr uint8_t pinOf(InputDevice sp) { return static_cast<uint8_t>(sp); }
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void resetExperiment();
-
-void printResults();
-
-void setup()
-{
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.backlight();
-
-  for (size_t i = 0; i < SENSOR_COUNT; ++i)
-    pinMode(pinOf(static_cast<InputDevice>(i)), INPUT);
-  pinMode(InputDevice::PushButton, INPUT_PULLUP);
-
-  resetExperiment();
-}
-
-void loop()
-{
-  static bool isExperimentRunning = false;
-  static unsigned long startTime = 0;
-
-  if (!isExperimentRunning && !digitalRead(InputDevice::PushButton))
-  {
-    delay(50);
-    if (digitalRead(InputDevice::PushButton))
-      return;
-    isExperimentRunning = true;
-    startTime = micros();
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Eksperimen mulai");
-  }
-
-  if (!isExperimentRunning)
-    return;
-
-  unsigned long elapsed = micros() - startTime;
-
-  bool states[SENSOR_COUNT];
-  for (size_t i = 0; i < SENSOR_COUNT; ++i)
-  {
-    bool input = (digitalRead(pinOf(static_cast<InputDevice>(i))));
-    if (i < 3)
-      states[i] = !input;
-    states[i] = input;
-  }
-
-  for (size_t i = 0; i < SENSOR_COUNT; ++i)
-  {
-    if (states[i] && !triggered[i])
-    {
-      timeStamps[i] = (i == 0 ? 0 : elapsed);
-      triggered[i] = true;
-      if (i > 0)
-      {
-        size_t idx = i - 1;
-        velocities[idx] = float(DISTANCES_MM[idx]) /
-                          ((timeStamps[i] - timeStamps[i - 1]) * 1e-6);
-      }
-      if (i == SENSOR_COUNT - 1)
-      {
-        printResults();
-        resetExperiment();
-        isExperimentRunning = false;
-      }
-      break;
-    }
-  }
-}
-
-void resetExperiment()
+void ResetExperiment()
 {
   memset(triggered, 0, sizeof(triggered));
+  memset(velocities, 0, sizeof(velocities));
   memset(timeStamps, 0, sizeof(timeStamps));
-  for (auto &v : velocities)
-    v = 0;
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Alat siap pakai");
 }
 
-void printResults()
+void PrintResults()
 {
   Serial.println("START_DATA");
   for (size_t i = 0; i < SENSOR_COUNT; i++)
@@ -145,4 +72,71 @@ void printResults()
   lcd.setCursor(0, 1);
   lcd.print("Vis: ");
   lcd.print(viscosity, 2);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.backlight();
+
+  for (auto i : Sensors)
+    pinMode(i, INPUT);
+  pinMode(InputDevice::PushButton, INPUT_PULLUP);
+
+  ResetExperiment();
+}
+
+void loop()
+{
+  static bool isExperimentRunning = false;
+  static unsigned long startTime = 0;
+
+  if (!isExperimentRunning && digitalRead(InputDevice::PushButton))
+  {
+    delay(50);
+    if (!digitalRead(InputDevice::PushButton))
+      return;
+    isExperimentRunning = true;
+    startTime = micros();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Eksperimen mulai");
+  }
+
+  if (!isExperimentRunning)
+    return;
+
+  unsigned long elapsed = micros() - startTime;
+
+  bool states[SENSOR_COUNT];
+  for (size_t i = 0; i < SENSOR_COUNT; i++)
+  {
+    bool input = digitalRead(Sensors[i]);
+    if (i < 3)
+      states[i] = !input;
+    states[i] = input;
+  }
+
+  for (size_t i = 0; i < SENSOR_COUNT; i++)
+  {
+    if (states[i] && !triggered[i])
+    {
+      timeStamps[i] = (i == 0 ? 0 : elapsed);
+      triggered[i] = true;
+      if (i > 0)
+      {
+        size_t idx = i - 1;
+        velocities[idx] = float(DISTANCES_MM[idx]) /
+                          ((timeStamps[i] - timeStamps[i - 1]) * 1e-6);
+      }
+      if (i == SENSOR_COUNT - 1)
+      {
+        PrintResults();
+        ResetExperiment();
+        isExperimentRunning = false;
+      }
+      break;
+    }
+  }
 }
